23d8df1bc9130fd7ae10743e1598fb39941c58ba
Olivier Sené <sene.olivier.pro@gmail.com>
2023-12-04 13:24:40 +0100

    Initialisation du nouveau composant de navigation en version mobile / Initialisation of the new mobile navigation  React Component

---
+++ src/components/MobileNav/MobileNav.tsx
@@ -0,0 +1,13 @@
+import './MobileNav.scss';
+
+function MobileNav() {
+  return (
+    <div className="MobileNav">
+      <div className="MobileNav_Search"></div>
+      <div className="MobileNav_Publish"></div>
+      <div className="MobileNav_Account"></div>
+    </div>
+  );
+}
+
+export default MobileNav;
