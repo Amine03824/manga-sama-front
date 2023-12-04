2cc08094a13c2a0e39183a4d6e25a8a5be74ef6b
Olivier Sené <sene.olivier.pro@gmail.com>
2023-12-04 14:51:16 +0100

    Barre de navigation mobile terminée / Mobile navigation version done

--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -1,28 +1,78 @@
+import { useState } from 'react';
 import './MobileNav.scss';
+import clsx from 'clsx';
 
 function MobileNav() {
+  const [clickedButton, setClickedButton] = useState(null);
+
+  function handleClickedButton(buttonType) {
+    setClickedButton(clickedButton === buttonType ? null : buttonType);
+  }
+
   return (
     <div className="MobileNav">
-      <button className="MobileNav__element">
+      <button
+        className={clsx('MobileNav__element', {
+          active: clickedButton === 'search',
+        })}
+        onClick={() => handleClickedButton('search')}
+        type="button"
+      >
         <img
-          src="public\assets\icons\MobileNavSearchIcon.png"
+          src={`public/assets/icons/MobileNavSearchIcon${
+            clickedButton === 'search' ? 'Red' : ''
+          }.png`}
           className="MobileNav__icon"
         />
-        <p className="MobileNav__icon_title">Rechercher</p>
+        <p
+          className={clsx('MobileNav__icon_title', {
+            active: clickedButton === 'search',
+          })}
+        >
+          Rechercher
+        </p>
       </button>
-      <button className="MobileNav__element">
+      <button
+        className={clsx('MobileNav__element', {
+          active: clickedButton === 'publish',
+        })}
+        onClick={() => handleClickedButton('publish')}
+        type="button"
+      >
         <img
-          src="public\assets\icons\MobileNavPublishIcon.png"
+          src={`public/assets/icons/MobileNavPublishIcon${
+            clickedButton === 'publish' ? 'Red' : ''
+          }.png`}
           className="MobileNav__icon"
         />
-        <p className="MobileNav__icon_title">Publier</p>
+        <p
+          className={clsx('MobileNav__icon_title', {
+            active: clickedButton === 'publish',
+          })}
+        >
+          Publier
+        </p>
       </button>
-      <button className="MobileNav__element">
+      <button
+        className={clsx('MobileNav__element', {
+          active: clickedButton === 'account',
+        })}
+        onClick={() => handleClickedButton('account')}
+        type="button"
+      >
         <img
-          src="public\assets\icons\MobileNavAccountIcon.png"
+          src={`public/assets/icons/MobileNavAccountIcon${
+            clickedButton === 'account' ? 'Red' : ''
+          }.png`}
           className="MobileNav__icon"
         />
-        <p className="MobileNav__icon_title">Mon compte</p>
+        <p
+          className={clsx('MobileNav__icon_title', {
+            active: clickedButton === 'account',
+          })}
+        >
+          Mon compte
+        </p>
       </button>
     </div>
   );
