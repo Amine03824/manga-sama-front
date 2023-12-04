d88c44799817089e69026ac7117b7e825e000fc1
Olivier Sené <sene.olivier.pro@gmail.com>
2023-12-04 14:20:44 +0100

    Structuration de la barre de navigation mobile / Structuring mobile navigation

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import MobileNav from '../MobileNav/MobileNav';
 
 function App() {
   return (
     <div>
       <Header />
       <Footer />
+      <MobileNav />
     </div>
   );
 }
--- src/components/MobileNav/MobileNav.tsx
+++ src/components/MobileNav/MobileNav.tsx
@@ -3,9 +3,27 @@ import './MobileNav.scss';
 function MobileNav() {
   return (
     <div className="MobileNav">
-      <div className="MobileNav_Search"></div>
-      <div className="MobileNav_Publish"></div>
-      <div className="MobileNav_Account"></div>
+      <button className="MobileNav__element">
+        <img
+          src="public\assets\icons\MobileNavSearchIcon.png"
+          className="MobileNav__icon"
+        />
+        <p className="MobileNav__icon_title">Rechercher</p>
+      </button>
+      <button className="MobileNav__element">
+        <img
+          src="public\assets\icons\MobileNavPublishIcon.png"
+          className="MobileNav__icon"
+        />
+        <p className="MobileNav__icon_title">Publier</p>
+      </button>
+      <button className="MobileNav__element">
+        <img
+          src="public\assets\icons\MobileNavAccountIcon.png"
+          className="MobileNav__icon"
+        />
+        <p className="MobileNav__icon_title">Mon compte</p>
+      </button>
     </div>
   );
 }
