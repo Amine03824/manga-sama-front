0c1fa4abb5fdf268b5418fb331534a18bf51837a
Houd <houd.alami-marrouni@oclock.school>
2023-12-04 10:28:10 +0100

    résolution conflit merge

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,11 @@
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 
 function App() {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-=======
-
-
-
+/* eslint-disable react/no-unescaped-entities */
 import './Footer.scss';
 
 function Footer() {
@@ -12,35 +8,33 @@ function Footer() {
         <div className="Footer__top_container-left">
           <ul className="Footer__list">
             <li className="Footer__list_item">
-              <a href="#">Mentions légales</a>
+              <a href="/">Mentions légales</a>
             </li>
             <li className="Footer__list_item">
-              <a href="#">Politique de confidentialité</a>
+              <a href="/">Politique de confidentialité</a>
             </li>
             <li className="Footer__list_item">
-              <a href="#">C.G.V.</a>
+              <a href="/">C.G.V.</a>
             </li>
           </ul>
         </div>
         <div className="Footer__top_container-right">
           <ul className="Footer__list">
             <li className="Footer__list_item">
-              <a href="#">L'équipe</a>
+              <a href="/">L'équipe</a>
             </li>
             <li className="Footer__list_item">
-              <a href="#">Nous contacter</a>
+              <a href="/">Nous contacter</a>
             </li>
           </ul>
         </div>
       </div>
 
       <div className="Footer__bottom_container">
-        <a href="#">Manga Sama - tous droits réservés 2023</a>
+        <a href="/">Manga Sama - tous droits réservés 2023</a>
       </div>
     </footer>
   );
 }
 
 export default Footer;
-
->>>>>>> 34498133f8f9b773e467bc43561ff7b23c13ab46
