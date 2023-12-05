eb440f111c5ce4d2ec232ec49907c07d3fa51378
Houd <houd.alami-marrouni@oclock.school>
2023-12-05 11:38:45 +0100

    login page done

--- src/routes/Login/Login.tsx
+++ src/routes/Login/Login.tsx
@@ -1,42 +1,54 @@
 import { Link } from 'react-router-dom';
 import './Login.scss';
+import Footer from '../../components/Footer/Footer';
+import Page from '../../components/Page/Page';
 
 function Login() {
   return (
-    <div className="login">
-      <div className="login__area">
-        <h2 className="login__area-title">Connexion</h2>
-        <div className="login__area-fields">
-          <form action="/" method="post" className="login__area-form">
-            <ul className="login__area-lists">
-              <li className="login__area-item">
-                <input type="text" placeholder="Nom d'utilisateur" />
-              </li>
-              <li className="login__area-item">
-                <input type="text" placeholder="Mot de passe" />
-              </li>
-            </ul>
-          </form>
-        </div>
-        <div className="login__area-valid-login">
-          <button type="button" className="login__area-button">
-            Se connecter
-            <img
-              src="public/assets/icons/CheckMarkIcon.png"
-              alt="login-icon"
-              className="login__area-button-icon"
-            />
-          </button>
-          <Link to="/" className="login__area-password">
-            Mot de passe oublié ?
+    <Page>
+      <div className="login">
+        <div className="login__area">
+          <h2 className="login__area-title">Connexion</h2>
+          <div className="login__area-fields">
+            <form action="/" method="post" className="login__area-form">
+              <ul className="login__area-lists">
+                <li className="login__area-item">
+                  <input type="text" placeholder="Nom d'utilisateur" />
+                </li>
+                <li className="login__area-item">
+                  <input type="text" placeholder="Mot de passe" />
+                </li>
+              </ul>
+            </form>
+          </div>
+          <div className="login__area-valid-login">
+            <button type="button" className="login__area-button">
+              Se connecter
+              <img
+                src="public/assets/icons/CheckMarkIcon.png"
+                alt="login-icon"
+                className="login__area-button-icon"
+              />
+            </button>
+            <Link to="/" className="login__area-password">
+              Mot de passe oublié ?
+            </Link>
+          </div>
+          <Link to="/signup" className="login__area-signup">
+            Pas encore membre ?
+            <span className="login__area-signup-link">Créer un compte</span>{' '}
           </Link>
         </div>
-        <Link to="/signup" className="login__area-signup">
-          Pas encore membre ?
-          <span className="login__area-signup-link">Créer un compte</span>{' '}
-        </Link>
+        <div className="login__logo-area">
+          <img
+            src="public/assets/logo/logo.png"
+            alt="logo-manga-sama"
+            className="login__logo"
+          />
+        </div>
       </div>
-    </div>
+      <Footer />
+    </Page>
   );
 }
 
