eaa986d2ed264ed2d77685cb193d529bccf6616b
Tanguy <ysteniag@gmail.com>
2023-12-06 13:47:41 +0100

    Ajout de axios ainsi que du createAsyncThunk pour récupérer les articles

--- src/store/reducers/article.ts
+++ src/store/reducers/article.ts
@@ -1,18 +1,39 @@
 /* eslint-disable import/no-named-as-default */
-import { createSlice } from '@reduxjs/toolkit';
-import { ArticleState } from '../../@types';
-import data from '../../data/data';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { Article, ArticleState } from '../../@types';
+import articles from '../../data/data';
 
 const initialState: ArticleState = {
-  list_articles: data,
+  list_articles: articles,
   error: null,
   isLoading: true,
 };
+
+export const getArticles = createAsyncThunk('articles/fetch', async () => {
+  const { data } = await axios.get<Article[]>('urlfetch');
+  return data;
+});
 
 const articleSlice = createSlice({
   name: 'article',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getArticles.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(getArticles.rejected, (state) => {
+        state.error = 'Problème lors de la récupération des articles';
+        state.isLoading = false;
+      })
+      .addCase(getArticles.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.list_articles = action.payload;
+      });
+  },
 });
 
 export default articleSlice.reducer;
