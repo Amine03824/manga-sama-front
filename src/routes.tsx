ce9e915f6eec03ff802a4acd56198ef6e48b2db5
Olivier Sené <sene.olivier.pro@gmail.com>
2023-12-07 16:37:19 +0100

    Création de la page Create Article / Create Article page create

--- src/routes.tsx
+++ src/routes.tsx
@@ -59,7 +59,7 @@ export const router = createBrowserRouter([
         element: <Category />,
       },
       {
-        path: 'article/create',
+        path: 'createarticle',
         element: <CreateArticle />,
       },
     ],
--- src/routes/CreateArticle/CreateArticle.tsx
+++ src/routes/CreateArticle/CreateArticle.tsx
@@ -1,82 +1,113 @@
+import React, { useState } from 'react';
 import Footer from '../../components/Footer/Footer';
 import Page from '../../components/Page/Page';
 import './CreateArticle.scss';
 
 function CreateArticle() {
+  // Utilisation des useState pour gérer les valeurs des inputs
+  const [title, setTitle] = useState('');
+  const [volume, setVolume] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [condition, setCondition] = useState('');
+
+  // Gestion de l'input de type select entre les différentes valeurs ("acceptable", "très bon", "neuf")
+  const handleConditionChange = (e) => {
+    setCondition(e.target.value);
+  };
+
+  // Gère la soumission du formulaire
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('Données soumises :', {
+      title,
+      volume,
+      price,
+      description,
+      condition,
+    });
+  };
+
   return (
     <Page>
-      <div className="Article">
-        <div className="Article__container_top">
-          <div className="Article__container_top-left">
-            <img
-              className="Article__container_top-left-img"
-              src="public\assets\icons\naruto01.jpg"
-              alt="image de couverture du manga"
-            ></img>
-          </div>
-          <div className="Article__container_top-right">
-            <div className="Article__container_top-right-title">
-              <h2 className="Article__container_top-right-title-booktitle">
-                Naruto
-              </h2>
-              <h4 className="Article__container_top-right-title-booksubtitle">
-                Volume 3
-              </h4>
-            </div>
+      <h2 className="CreateArticle__title">Créer une nouvelle annonce</h2>
+      <div className="CreateArticle__container">
+        <div className="CreateArticle__container_left">
+          <img src="\assets\icons\naruto01.jpg"></img>
+        </div>
+        <div className="CreateArticle__container_right">
+          <form onSubmit={handleSubmit} className="CreateArticle__form">
+            <label htmlFor="title" className="CreateArticle__form_label">
+              Titre de l'annonce :
+            </label>
+            <input
+              className="CreateArticle__form_input"
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              required
+            />
 
-            <p className="Article__container_top-right-price">59 €</p>
+            <label htmlFor="volume" className="CreateArticle__form_label">
+              Volume :
+            </label>
+            <input
+              className="CreateArticle__form_input"
+              type="text"
+              id="volume"
+              value={volume}
+              onChange={(e) => setVolume(e.target.value)}
+              required
+            />
 
-            <div className="Article__container_top-right-description">
-              <p className="Article__container_top-right-description-title">
-                Description :
-              </p>
-              <p className="Article__container_top-right-description-content">
-                Livre de type Naruto avec des mangas de type japonais se lisant
-                à l’envers. Fait en papier.
-              </p>
-            </div>
+            <label htmlFor="price" className="CreateArticle__form_label">
+              Prix:
+            </label>
+            <input
+              className="CreateArticle__form_input"
+              type="text"
+              id="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              required
+            />
 
-            <div className="Article__container_top-right-state">
-              <h5 className="Article__container_top-right-state-title">
-                Etat de l'article :
-              </h5>
-              <p className="Article__container_top-right-state-described">
-                Très bon état
-              </p>
-            </div>
-            <div className="Article__container_top-right-soldby">
-              <p className="Article__container_top-right-soldby-title">
-                Vendu par :
-              </p>
-              <p className="Article__container_top-right-soldby-sellername">
-                Jean_guy_17
-              </p>
-            </div>
-            <button className="Article__container_top-right-purchase-btn">
-              Acheter{' '}
-              <img
-                className="Article__container_top-right-img"
-                src="public\assets\icons\cart-icon-32px.png"
-                alt="icône de caddy"
-              ></img>
+            <label htmlFor="description" className="CreateArticle__form_label">
+              Description de l'article :
+            </label>
+            <textarea
+              className="CreateArticle__form_input"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              required
+            ></textarea>
+
+            <label htmlFor="condition" className="CreateArticle__form_label">
+              État de l'article :
+            </label>
+            <select
+              className="CreateArticle__form_input"
+              id="condition"
+              value={condition}
+              onChange={handleConditionChange}
+              required
+            >
+              <option value="" disabled>
+                Sélectionnez l'état
+              </option>
+              <option value="acceptable">Acceptable</option>
+              <option value="très bon">Très bon</option>
+              <option value="neuf">Neuf</option>
+            </select>
+
+            <button className="CreateArticle__form_btn" type="submit">
+              Publier mon annonce <img src="\assets\icons\add.png"></img>
             </button>
-          </div>
+          </form>
         </div>
-        <div className="Article__container_bottom">
-          <p className="Article__container_bottom-title">Description :</p>
-          <p className="Article__container_bottom-content">
-            Livre de type Naruto avec des mangas de type japonais se lisant à
-            l’envers. Fait en papier.
-          </p>
-        </div>
-        <button className="Article__purchase_btn">
-          Publier mon annonce{' '}
-          <img
-            className="Article__purchase_img"
-            src="public\assets\icons\cart-icon-32px.png"
-            alt="icône de caddy"
-          ></img>
-        </button>
       </div>
 
       <Footer />
