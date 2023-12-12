85168fb6a4b85f7f5ae4c3d429ba954f299d5e1e
Houd <houd.alami-marrouni@oclock.school>
2023-12-12 11:12:52 +0100

    creation route user

---
+++ src/store/reducers/userPage.ts
@@ -0,0 +1,77 @@
+/* eslint-disable import/prefer-default-export */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { TArticle, TUserArticle } from '../../@types';
+
+type UserPageState = {
+  userPageArticle: TArticle[];
+  userPageInfo: TUserArticle | null;
+  userId: number | null;
+  isLoading: boolean;
+  messageUserPage: string;
+  errorUserPage: string | null;
+};
+
+const initialState: UserPageState = {
+  userPageArticle: [],
+  userPageInfo: null,
+  userId: null,
+  isLoading: false,
+  messageUserPage: '',
+  errorUserPage: null,
+};
+
+export const getArticleByUser = createAsyncThunk(
+  'userArticle/fetch',
+  async (userId: number) => {
+    const { data } = await axios.get<TArticle[]>(
+      `http://localhost:3000/article/user/${userId}`
+    );
+    return data;
+  }
+);
+
+export const getUserById = createAsyncThunk(
+  'userInfo/fetch',
+  async (userId: number) => {
+    const { data } = await axios.get<TUserArticle>(
+      `http://localhost:3000/user/${userId}`
+    );
+    return data;
+  }
+);
+
+const userPageReducer = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      // Gestion du fetch afin de recuperer les article selon le user
+      .addCase(getArticleByUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getArticleByUser.rejected, (state) => {
+        state.isLoading = true;
+        state.errorUserPage = "Cet utilisateur n'a aucun article";
+      })
+      .addCase(getArticleByUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.userPageArticle = action.payload;
+      })
+      // gestion du fetch afin de recuperer un user par son id
+      .addCase(getUserById.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getUserById.rejected, (state) => {
+        state.isLoading = true;
+        state.errorUserPage = ' Aucun utilisateur trouvé ';
+      })
+      .addCase(getUserById.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.userPageInfo = action.payload;
+      });
+  },
+});
+
+export default userPageReducer.reducer;
