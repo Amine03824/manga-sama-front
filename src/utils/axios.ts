73988a4e0f42422975f18734e05d86c335cac0ce
Amine03824 <amine.boukraa@protonmail.com>
2023-12-20 13:57:40 +0100

    📃 docs(modification de l'url de base pour le fetch):

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -6,7 +6,7 @@ import { LocalStorage } from './LocalStorage';
 // On créer une instance d'axios.
 // Cela permet de spécifier une configuration pour toutes les requêtes effectuées avec cette instance
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://manga-samas.onrender.com',
 });
 // Je rajoute un intercepteur, cela me permet avant que la requête soit faite de modifier la configuration
 axiosInstance.interceptors.request.use((config) => {
